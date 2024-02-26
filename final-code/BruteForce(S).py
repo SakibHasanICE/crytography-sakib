bf3f6ff7af893fb08bdc03d2c6a0b57a93a10d3f
Sakib Hasan <81642704+SakibHasanICE@users.noreply.github.com>
2024-02-26 22:42:09 +0600

    Add files via upload

---
+++ final-code/BruteForce(S).py
@@ -0,0 +1,21 @@
+def brute_force_decrypt(ciphertext):
+    for shift in range(26):
+        decrypted_text = caesar_decrypt(ciphertext, shift)
+        print(f"Shift {shift}: {decrypted_text}")
+
+def caesar_decrypt(ciphertext, shift):
+    decrypted_text = ""
+    for char in ciphertext:
+        if char.isalpha():
+            if char.islower():
+                decrypted_text += chr((ord(char) - shift - ord('a')) % 26 + ord('a'))
+            else:
+                decrypted_text += chr((ord(char) - shift - ord('A')) % 26 + ord('A'))
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+
+ciphertext = str(input("enter cipher text :"))
+print("Brute Force Decryption for Caesar Cipher:", ciphertext)
+brute_force_decrypt(ciphertext)
